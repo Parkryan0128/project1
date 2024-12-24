2b0f5f913c8a8fc1cab177ff9a6e782826b243e3
Yushin Nam <lojnys@hotmail.com>
2024-12-24 02:17:14 -0800

    created implementation for Stack and Queue and in the progress of making a stack algorithm for BEDMAS

--- src/util/BEDMAS.js
+++ src/util/BEDMAS.js
@@ -1,1 +1,35 @@
+import { Stack } from './stack.js'
+import { Queue } from './queue.js'
 
+
+
+// helper function to determine if a given string is
+// is an operand or a number
+function isNumber(str) {
+    return !isNaN(str) && str.trim() !== ""
+}
+
+// helper function to return the precedence of a given operator
+function precedence(operator) {
+    if (operator == '+' || operator == '-') return 1
+    if (operator == '*' || operator == '/') return 2
+    if (operator == '^') return 3
+    return 0
+}
+
+// evaluates the given string of expression
+function evaluate(str) {
+    let stack = new Stack()
+    let queue = new Queue()
+
+
+    for (let i = 0; i < str.length; i++) {
+        if (isNumber(str[i])) {
+            queue.enqueue(Number(str[i]))
+        } else if (str[i] == '(') {
+            stack.push(str[i])
+        } else if (str[i] == ')') {
+            // will do tmr
+        } 
+    }
+}
---
+++ src/util/queue.js
@@ -0,0 +1,54 @@
+
+// This class is the implementation of Queue data structure
+
+export class Queue {
+
+    // Array is used to implement queue
+    constructor() {
+        this.items = [];
+        this.frontIndex = 0;
+        this.backIndex = 0;
+    }
+
+    // the item is added to the back of the queue
+    // and the index is incremented by 1
+    enqueue(item) {
+        this.items[this.backIndex] = item
+        this.backIndex++
+    }
+
+    // the item is removed from the front of the queue
+    dequeue(item) {
+        const first_item = this.items[this.frontIndex]
+        delete this.items[this.frontIndex]
+        this.frontIndex++
+        return item
+    }
+
+    // returns the first item in the queue
+    peek() {
+        return this.items[this.frontIndex]
+    }
+
+    // returns true if the queue is empty
+    isEmpty() {
+        return this.items.length == 0
+    }
+
+    // returns the length of the queue
+    size() {
+        return this.items.length
+    }
+
+}
+
+
+// test
+// let queue = new Queue()
+
+// queue.enqueue(4)
+// console.log(queue.size())
+// queue.dequeue()
+// queue.enqueue(1)
+// queue.enqueue(2)
+// console.log(queue.isEmpty())
