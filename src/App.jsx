b042471c79334f47e1711f6b9d5ad9556f8e7367
andre108 <andre108@students.cs.ubc.ca>
2024-12-24 15:44:21 -0800

    Input skeleton created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Input from './components/Input.jsx'
+import Input from './components/InputBox.jsx'
 
 function App() {
   return <div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import './Input.css'
+import { useState } from 'react'
 
 
 function Input() {
-  return <div>This is Input</div>
+    let [expression, displayInput] = useState(" ");
+    let [isFocused, setIsFocused] = useState(false);
+    let [showGraph, setShowGraph] = useState(true);
+    let [isAlive, setIsAlive] = useState(true);
+
+    let hasValue = expression.trim() !== '';
+
+    return (
+        <div
+            className={`Input ${isFocused ? 'focused' : ''}`}>
+            <div
+                className={`OnOff ${isFocused ? 'focused' : ''}`}>
+                1
+                <button
+                    className={`OnOffButton 
+                        ${hasValue ? '' : 'noValue'} 
+                        ${showGraph ? '' : 'showGraphFalse'}`}
+                    onClick={() => setShowGraph(!showGraph)}>
+                </button>
+            </div>
+            {expression}
+            <input
+                className="UserInput"
+                value={expression}
+                onChange={(e) => displayInput(e.target.value)}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}>
+            </input>
+            <button 
+            className='deleteButton'
+            onClick={() => setIsAlive(false)}>x</button>
+        </div>
+    );
 }
 
 export default Input
