e2404cdb84a46cd4477046d8ff5b2e0300cdc273
Yushin Nam <lojnys@hotmail.com>
2024-12-30 20:53:14 -0800

    fixed problem with deleting span when clicked input slot

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import GraphCanvas from './components/GraphCanvas.jsx'
 import Input from './components/Input.jsx'
+import InputContent from './components/inputtest.jsx'
 
 function App() {
     
     return <div>
         <Input />
-        <GraphCanvas />
+        {/* <GraphCanvas /> */}
+        <InputContent />
     </div>
 }
 
