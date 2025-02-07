b2dc268efac15729b03a78f6aa550f1646843165
Yushin Nam <lojnys@hotmail.com>
2025-02-06 16:31:38 -0800

    backspace and arrow logic complete

--- src/components/InputIntegral.jsx
+++ src/components/InputIntegral.jsx
@@ -9,8 +9,6 @@ function InputIntegral() {
         'cursor'
     ])
     let [processedInput, setprocessedInput] = useState([])
-    let [type, setType] = useState('text')
-    let [tempStr, setStr] = useState("")
     let [isFocused, setIsFocused] = useState(false)
 
 
@@ -88,18 +86,11 @@ function InputIntegral() {
 
         // deletes the existing processedInput and focus the previous block
         if (e.key == "Backspace") {
-            
-            // when backspace is pressed in value and it has no numbers in it,
-            // then we move the cursor to the upperbound
-            if (copy[cursorIndex-1] === '_INT_VALUE_BRACKET_OPEN_') {
-                let final = insertAt(copy, copy.indexOf("_INT_UPPER_BRACKET_CLOSE_"), 'cursor')
-                final.splice(cursorIndex+1, 1)
-                setUserInput(final)
-            } 
+        
             // when backspace is pressed in upperbound and it has no numbers in it, 
             // then we remove the integral (as well as all the components; upperbound, 
             // lowerbound, and value)
-            else if (copy[cursorIndex-1] === '_INT_UPPER_BRACKET_OPEN_') {
+            if (copy[cursorIndex-1] === '_INT_UPPER_BRACKET_OPEN_') {
 
                 let upperStart = cursorIndex - 1;
                 let upperEnd;
@@ -129,7 +120,7 @@ function InputIntegral() {
                     }
                 })
 
-                copy.splice(0, 3)
+                copy.splice(cursorIndex-4, 3)
                 copy = deleteAt(copy, upperStart-3)
                 copy = deleteAt(copy, upperEnd-4)
                 copy = deleteAt(copy, lowerStart-5)
@@ -139,10 +130,90 @@ function InputIntegral() {
 
                 setUserInput(copy)
             } 
-            // when backspace is pressed in lowerbound and it has no numbers in it,
-            // then we remove the integral (as well as all the components) - same logic as upperbound
             else if (copy[cursorIndex-1] === '_INT_LOWER_BRACKET_OPEN_') {
-                
+                let upperStart;
+                let upperEnd;
+                let lowerStart = cursorIndex - 1;
+                let lowerEnd;
+                let valueStart;
+                let valueEnd;
+
+                pairs.forEach((item) => {
+                    if (item[0] === lowerStart) {
+                        lowerEnd = item[1]
+                        valueStart = item[1] + 1
+                        upperEnd = item[0] - 1
+                    }
+                })
+
+                pairs.forEach((item) => {
+                    if (item[0] === valueStart) {
+                        valueEnd = item[1];
+                    }
+                })
+
+                pairs.forEach((item) => {
+                    if (item[1] === upperEnd) {
+                        upperStart = item[0];
+                    }
+                })
+
+                copy.splice(cursorIndex-6, 3)
+                copy = deleteAt(copy, upperStart-3)
+                copy = deleteAt(copy, upperEnd-4)
+                copy = deleteAt(copy, lowerStart-5)
+                copy = deleteAt(copy, lowerEnd-6)
+                copy = deleteAt(copy, valueStart-7)
+                copy = deleteAt(copy, valueEnd-8)
+
+                setUserInput(copy)
+
+            }
+            else if (copy[cursorIndex-1] === '_INT_VALUE_BRACKET_OPEN_' &&
+                cursorIndex+1 < copy.length-1) {
+                let upperStart;
+                let upperEnd;
+                let lowerStart;
+                let lowerEnd;
+                let valueStart = cursorIndex - 1;
+                let valueEnd;
+
+                pairs.forEach((item) => {
+                    if (item[0] === valueStart) {
+                        valueEnd = item[1]
+                        lowerEnd = item[0] - 1
+                    }
+                })
+
+                pairs.forEach((item) => {
+                    if (item[1] === lowerEnd) {
+                        lowerStart = item[0];
+                        upperEnd = item[0] - 1;
+                    }
+                })
+
+                pairs.forEach((item) => {
+                    if (item[1] === upperEnd) {
+                        upperStart = item[0];
+                    }
+                })
+
+                copy.splice(cursorIndex-8, 3)
+                copy = deleteAt(copy, upperStart-3)
+                copy = deleteAt(copy, upperEnd-4)
+                copy = deleteAt(copy, lowerStart-5)
+                copy = deleteAt(copy, lowerEnd-6)
+                copy = deleteAt(copy, valueStart-7)
+                copy = deleteAt(copy, valueEnd-8)
+
+                setUserInput(copy)
+            }
+            // when backspace is pressed in value and it has no numbers in it,
+            // then we move the cursor to the upperbound
+            else if (copy[cursorIndex-1] === '_INT_VALUE_BRACKET_OPEN_') {
+                let final = insertAt(copy, copy.indexOf("_INT_UPPER_BRACKET_CLOSE_"), 'cursor')
+                final.splice(cursorIndex+1, 1)
+                setUserInput(final)
             }
 
             else {
@@ -152,67 +223,52 @@ function InputIntegral() {
         }
 
         if (e.key == 'ArrowLeft') {
-            const updated = swapItems(userInput, cursorIndex-1, cursorIndex)
-            setUserInput(updated)
+
+            if (cursorIndex <= 0) {
+                setUserInput(copy)
+            } else if (copy[cursorIndex-1] === "_INT_UPPER_BRACKET_OPEN_") {
+                const updated = insertAt(copy, cursorIndex-4, 'cursor')
+                cursorIndex = updated.lastIndexOf("cursor")
+                updated.splice(cursorIndex, 1)
+                setUserInput(updated)
+            } else if (copy[cursorIndex-1] === '_INT_LOWER_BRACKET_OPEN_') {
+                let updated = insertAt(copy, cursorIndex-2, 'cursor')
+                cursorIndex = updated.lastIndexOf('cursor')
+                updated.splice(cursorIndex, 1)
+                setUserInput(updated)
+            } else if (copy[cursorIndex-1] === '_INT_VALUE_BRACKET_OPEN_') {
+                let updated = insertAt(copy, cursorIndex-2, 'cursor')
+                cursorIndex = updated.lastIndexOf('cursor')
+                updated.splice(cursorIndex, 1)
+                setUserInput(updated)
+            } else {
+                const updated = swapItems(copy, cursorIndex-1, cursorIndex)
+                setUserInput(updated)
+            }
         }
 
         if (e.key == 'ArrowRight') {
-            const updated = swapItems(userInput, cursorIndex, cursorIndex+1)
-            setUserInput(updated)
+
+            if (cursorIndex >= copy.length -1 ) {
+                setUserInput(copy)
+            } else if (copy[cursorIndex+1] === 'i' && copy[cursorIndex+2] === 'n' && copy[cursorIndex+3] === 't') {
+                let updated = insertAt(copy, cursorIndex+5, 'cursor')
+                updated.splice(cursorIndex, 1)
+                setUserInput(updated)
+            } else if (copy[cursorIndex+1] === '_INT_UPPER_BRACKET_CLOSE_') {
+                let updated = insertAt(copy, cursorIndex+3, 'cursor')
+                updated.splice(cursorIndex, 1)
+                setUserInput(updated)
+            } else if (copy[cursorIndex+1] === '_INT_LOWER_BRACKET_CLOSE_') {
+                let updated = insertAt(copy, cursorIndex+3, 'cursor')
+                updated.splice(cursorIndex,1)
+                setUserInput(updated)
+            } else {
+                const updated = swapItems(copy, cursorIndex, cursorIndex+1)
+                setUserInput(updated)
+            }
         }
      }
-
-    // function processInput() {
-    //     let arr = []
-    //     let i = 0
-    //     let copy = [...userInput]
-    //     const upper = []
-    //     const lower = ''
-    //     const value = ''
-
-    //     while (i < copy.length) {
-    //         if (copy[i] === 'cursor') {
-    //             arr.push({
-    //                 type: 'cursor',
-    //                 value: '\u00A0'
-    //             })
-    //             i++;
-    //         } else if (/^[a-zA-Z0-9+\-*()^=/_]+$/i.test(copy[i])) {
-    //             if (copy[i] === 'i' && copy[i+1] === 'n' && copy[i+2] === 't') {
-
-
-    //                 // need to extract all the values for the upperbound and lowerbound
-    //                 // if (userInput[i+3] === '(') {
-
-    //                 //     for (let k = 0; k < arr.length; k++) {
-    //                 //         upper += arr[i+4+k]
-
-    //                 //         if (userInput[i+4+k] === ')') {
-    //                 //             i = i+5+k
-    //                 //         }
-    //                 //     }
-    //                 // }
-
-    //                 arr.push({
-    //                     type: 'integral',
-    //                     value: [],
-    //                     upperBound: [{type:'text', value:'a'}],
-    //                     lowerBound: [{type:'text', value:'b'}]
-    //                 })
-    //                 i += 3
-    //                 // setType('upperBound')
-    //             } else {
-    //                 arr.push({
-    //                     type: 'text',
-    //                     value: copy[i]
-    //                 })
-    //                 i++
-    //             }
-    //         }
-    //     }
-    //     // setprocessedInput(arr)
-    //     return arr
-    // }
 
     function processInput(input) {
         let arr = []
