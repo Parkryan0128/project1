f29604373529504160ba3681c6b8483884fdb019
Donghwan Lee <dhwanyi@gmail.com>
2025-02-20 23:55:23 -0800

    Made the inputList resizable and added an input array for each row of inputs

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import InputList from '../components/InputList';
 import GraphCanvas from '../components/GraphCanvas';
 import './MainPage.css';
@@ -6,21 +6,79 @@ import './MainPage.css';
 const MainPage = () => {
     const [hidden, setHidden] = useState(false);
     const [graphWidth, setGraphWidth] = useState(window.innerWidth * 0.70);
+    const [inputValue, setInputValues] = useState([]);
+    const [inputWidth, setInputWidth] = useState(window.innerWidth * 0.30);
 
-    console.log(window.innerWidth);
+    const sidebarRef = useRef(null);
+    const resizerRef = useRef(null);
+    const startX = useRef(0);
+    const startWidth = useRef(0);
+    const isResizing = useRef(false);
+    
     useEffect(() => {
-        setGraphWidth(window.innerWidth * (hidden ? 1 : 0.7))
+        if (hidden) {
+            setInputWidth(0);
+            setGraphWidth(window.innerWidth);
+        } else {
+            setInputWidth(window.innerWidth * 0.30);
+            setGraphWidth(window.innerWidth * 0.70);
+        }
     }, [hidden]);
+
+    const handleInputChange = (rows) => {
+        setInputValues(rows);
+        console.log('userInput: ', rows);
+    }
+
+    const handleMouseDown = (e) => {
+        e.preventDefault();
+        isResizing.current = true;
+        startX.current = e.clientX;
+
+        let sbWidth = window.getComputedStyle(sidebarRef.current).width;
+        startWidth.current = parseInt(sbWidth, 10);
+
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    }
+
+    const handleMouseUp = () => {
+        isResizing.current = false;
+        // removew event mousemove && mouseup
+        document.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+    };
+
+    // Mouse Move Handler to Resize
+    const handleMouseMove = (e) => {
+        if (!isResizing) return;
+        let dx = e.clientX - startX.current;
+        let newWidth = startWidth.current + dx; // complete width
+
+        const minSidebarWidth = window.innerWidth * 0.15;
+        const maxSidebarWidth = window.innerWidth * 0.50;
+        
+        if (newWidth < minSidebarWidth) newWidth = minSidebarWidth;
+        if (newWidth > maxSidebarWidth) newWidth = maxSidebarWidth;
+
+        const newGraphWidth = window.innerWidth - newWidth;
+        if (newGraphWidth >= window.innerWidth * 0.40) {
+            setInputWidth(newWidth);
+            setGraphWidth(newGraphWidth);
+        }
+    };
+
 
     return (
         <div className='graph-layout'>
             {!hidden && (
-                <div className='input-section'>
-                    <InputList hidden={hidden} setHidden={setHidden} setGraphWidth={setGraphWidth}/>
+                <div className='input-section' ref={sidebarRef} style={{ width: inputWidth }}>
+                    <InputList hidden={hidden} setHidden={setHidden} setGraphWidth={setGraphWidth} onInputChange={handleInputChange}/>
+                    <div className="resizer" ref={resizerRef} onMouseDown={handleMouseDown} />
                 </div>
             )}
 
-            <div className='graph-section' style={{ width: hidden ? '100%' : '70%' }}>
+            <div className='graph-section' style={{ width: graphWidth }}>
                 <GraphCanvas graphWidth={graphWidth} />
                 {hidden && (
                     <button className='input-list__show-btn' onClick={() => setHidden(false)}>
