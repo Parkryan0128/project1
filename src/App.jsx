195d66d877dbaff7780474ad1b8148fad06871bc
shk020405 <andre108@students.cs.ubc.ca>
2025-02-27 18:28:26 -0800

    new file created

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import Input from './components/Input.jsx'
 import InputList from './components/InputList.jsx'
 import { derivative, differentiate } from './utils/Derivative.js'; // Import derivative function
 
+
+
 function App() {
     return <div>
       <Input/>
+      <></>
       </div>
 }
 
